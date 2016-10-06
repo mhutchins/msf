634cc5a9ad917957322b6588bde1d9f0cde4ffb4
Martin Hutchins <martin@localhost.localdomain>
2016-10-06 07:54:21 +0100

    Reorg and tidy

---
+++ main.h
@@ -0,0 +1,10 @@
+
+extern volatile time_t rtc_time;
+extern volatile time_t msf_time[2];
+
+time_t time_master(void);
+
+extern FILE display_led;
+extern FILE display_serial;
+extern FILE display_lcd;
+
